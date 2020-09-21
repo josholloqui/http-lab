358882628704d31a308840ed2e64c8841b07ae45
Frank Josh Olloqui <josh.olloqui@gmail.com>
2020-09-21 16:12:29 -0700

    adds color routes and their passing test also makes sure if the route doesnt exist it 404 not found with accompanying test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -16,4 +16,32 @@ describe('createResponse', () => {
     
     expect(response.text).toEqual('hello again');
   });  
+
+  it('returns red html when path is /red', async() => {
+    const response = await request(app)
+      .get('/red');
+    
+    expect(response.text).toEqual('<html><body><h1>red</h1></body></html>');
+  });
+
+  it('returns green html when path is /green', async() => {
+    const response = await request(app)
+      .get('/green');
+    
+    expect(response.text).toEqual('<html><body><h1>green</h1></body></html>');
+  });
+
+  it('returns blue html when path is /blue', async() => {
+    const response = await request(app)
+      .get('/blue');
+    
+    expect(response.text).toEqual('<html><body><h1>blue</h1></body></html>');
+  });
+
+  it('returns not found when path is not real /fake', async() => {
+    const response = await request(app)
+      .get('/fake');
+    
+    expect(response.text).toEqual('Not Found');
+  });
 });
