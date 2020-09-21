a801422692290c14514789777503d6b8a774cb1a
Frank Josh Olloqui <josh.olloqui@gmail.com>
2020-09-21 16:02:03 -0700

    adds echo and passing echo test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,18 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('createResponse', () => {
+  it('returns hi when path is /', async() => {
+    const response = await request(app)
+      .get('/');
+    
+    expect(response.text).toEqual('hi');
+  });
 
+  it('returns hi when path is /', async() => {
+    const response = await request(app)
+      .post('/echo')
+      .send('hello again');
+    
+    expect(response.text).toEqual('hello again');
+  });  
 });
